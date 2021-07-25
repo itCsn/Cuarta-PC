147cdac9ec8276a3e615ad6e51e31fc07939a0d1
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:50:39 -0500

    Update sinonimos.py

--- bic01w-tercera-practica-calificada-itCsn/sinonimos.py
+++ bic01w-tercera-practica-calificada-itCsn/sinonimos.py
@@ -2,34 +2,19 @@ DICCIONARIO_SINONIMOS = {}
 
 
 def registrar_sinonimo(entrada, sinonimo):
-    if entrada in DICCIONARIO_SINONIMOS.keys():
-        DICCIONARIO_SINONIMOS[entrada].append(sinonimo)
-    else:
-        lista_sinonimos = []
-        lista_sinonimos.append(sinonimo)
-        DICCIONARIO_SINONIMOS[entrada] = lista_sinonimos
+   
     return None
 
 
 
 def mostrar_diccionario():
-    entradas = len(DICCIONARIO_SINONIMOS.keys())
-    print(f"DICCIONARIO DE SINONIMOS ({entradas} entradas)")
-    for palabra in DICCIONARIO_SINONIMOS.keys():
-        mostart_sinonimos(palabra)
-
-        print("")
+    
 
 
 
 
 def mostrar_sinonimos(entrada):
-    for i in DICCIONARIO_SINONIMOS[palabra] :
-         longitud_primera_columna = " "*(15 - 1 - len(palabra))
-         longitud_segunda_columna = " "*(15 - len(i))
-         print(f"{palabra}:{longitud_primera_columna}{i}{longitud_segunda_columna}")
-
-    print("")
+    
 
 
 if __name__ == "__main__":
