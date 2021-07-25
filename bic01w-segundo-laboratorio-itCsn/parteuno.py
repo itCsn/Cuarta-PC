23ff8c6bb0cf699e996dd2ac755559d0e847ff93
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 22:08:04 -0500

    Update parteuno.py

--- bic01w-segundo-laboratorio-itCsn/parteuno.py
+++ bic01w-segundo-laboratorio-itCsn/parteuno.py
@@ -1,22 +1,7 @@
 def ejecutar():
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
-    costoCasa=int(input("Ingrese el costo de la casa: "))
-    salarioAnual=int(input("Ingrese su salario anual: "))
-    porcentajeDeAhorro=float(input("Ingrese el porcentaje de ahorro mensual: "))
-    ahorroMensual=salarioAnual*porcentajeDeAhorro/12
-    costoInicial=costoCasa/4
-    tasa=4/1200
-    interes=0
-    meses=1
-    monto=0
-    while monto<costoInicial:
-        ahorro=ahorroMensual+monto
-        interes=ahorro*tasa
-        monto=ahorro + interes
-        meses+=meses
 
-    print(f"Numero de meses: {meses}")
     # FIN
     return
 
