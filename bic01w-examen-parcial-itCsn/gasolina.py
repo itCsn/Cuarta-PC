cb0404264f5c615869451953e994d7a51732514b
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:54:19 -0500

    Update gasolina.py

--- bic01w-examen-parcial-itCsn/gasolina.py
+++ bic01w-examen-parcial-itCsn/gasolina.py
@@ -1,22 +1,7 @@
 def iniciar():
     # Ingrese la solucion en las lineas subsiguientes
     # INICIO
-    consumo_total = 0
-    tramo = 1
-    gasolina = float(input(f"Tramo {tramo}: Ingrese la gasolina consumida, en galones (-1 para terminar: "))
-    gasolina = round(gasolina, 2)
-    print(f"En el tramno {tramo}, el consumo de gasolina fue {gasolina} millas por galon. ")
-    consumo_total += gasolina
-    while gasolina != -1:
-        tramo += 1
-        gasolina = float(input(f"Tramo {tramo}: Ingrese la gasolina consumida, en galones (-1 para terminar: "))
-        gasolina = round(gasolina, 2)
-        consumo_total += gasolina
-        consumo_total = round(consumo_total,2)
-        print(f"En el tramno {tramo}, el consumo de gasolina fue {gasolina} millas por galon. ")
-
-    print(f"En {tramo} tramos, el consumo de gasolina fue {consumo_total} millas por galon. ")
- 
+   
     # FIN
     return
 
