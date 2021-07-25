1849a14d066beb6802061de2cae9432773835ba1
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:52:14 -0500

    Update tabla.py

--- bic01w-segundapractica-calificada-itCsn/tabla.py
+++ bic01w-segundapractica-calificada-itCsn/tabla.py
@@ -1,18 +1,4 @@
-#def mostrar_lista(lista_de_listas):
-#    for i in range(len(lista_de_listas))
-#    print("lista_de_listas[i]\t")
 
-def crear_lista(filas,columnas):
-    """
-    fila:int, numeros de listas internas
-    columnas:int, cantidad de números de las listas 
-    """
-    lista_externa = []
-    for i in range(filas):
-        lista_interna = []
-        for i in range(columnas):
-            lista_interna.append(i+1)
-        lista_externa.append(lista_interna)
  
 def iniciar():
     pass
