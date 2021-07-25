b635afb8085345ddd5290a9132471866a5a79e32
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:51:04 -0500

    Update temperaturas.py

--- bic01w-tercera-practica-calificada-itCsn/temperaturas.py
+++ bic01w-tercera-practica-calificada-itCsn/temperaturas.py
@@ -2,11 +2,6 @@ TEMPERATURAS_POR_DIA = {}
 
 
 def ingresar_temperaturas(dia_semana, numero_temperaturas):
-    lista_temperaturas = []
-    for dia in range(1, numero_temperaturas + 1):
-        temperatura = float(input(f"Ingrese temperatura {dia_semana} para el dia Lunes: "))
-        lista_temperaturas.append(temperatura)
-    TEMPERATURAS_POR_DIA[dia_semana] = lista_temperaturas
 
     return None
 
@@ -14,17 +9,7 @@ def ingresar_temperaturas(dia_semana, numero_temperaturas):
 
 
 def mostrar_reporte():
-    lista_dias = TEMPERATURAS_POR_DIA.keys()
-    lista_temperatura = TEMPERATURAS_POR_DIA.values()
-    for dia in lista_dias:
-        suma = 0
-        for temperatura in TEMPERATURAS_POR_DIA[dia]:
-            suma += temperatura
-        promedio = suma/len(lista_temperatura)
-        print("TEMPERATURA PROMEDIO POR DIA") 
-        espacios = " "*(12 - len(dia) - 1 - len(str(promedio)))
-        print(f"{dia}:{espacios}{promedio}")
-        print("")
+
 
 
 
