f4be26d248b0745154e111aa83ffa23587a47d5c
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:52:57 -0500

    Update binario.py

--- bic01w-primera-practica-calificada-itCsn/binario.py
+++ bic01w-primera-practica-calificada-itCsn/binario.py
@@ -1,25 +1,5 @@
 def iniciar():
-    # Ingrese la solucion en las lineas subsiguientes.
-    # INICIO
-    numeroBinario = input("Ingrese un numero en sistema binario:")
-    lista = list(numeroBinario)
-    posicion = len(lista) - 1
-    equivalente = 0
- 
-    for i in range(len(lista)):
- 
-        numero = int(lista[i])
     
-        while numero >= 2:
-            
-            print("El numero ingresado no es valido.")
-            numeroBinario = input("Ingrese un numero en sistema binario:")
-            lista = list(numeroBinario)
-            numero = int(lista[i])
-            
-        equivalente = equivalente + numero*2**posicion
-
-    print(f"El equivalente en decimal es {equivalente}")
     # FIN
     return
 
