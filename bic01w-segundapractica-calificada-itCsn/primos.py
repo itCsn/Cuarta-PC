760e377198d1fb7c275267d7c817289c4f9a3325
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:51:52 -0500

    Update primos.py

--- bic01w-segundapractica-calificada-itCsn/primos.py
+++ bic01w-segundapractica-calificada-itCsn/primos.py
@@ -1,37 +1,15 @@
 def generar_lista_booleanos(entero):
-    cantidad = entero + 1
-    lista = [] 
-    for i in range(cantidad):
-        lista.append(True)     
-        
-    return lista
+   
+   
 
 def marcar_como_falso(lista,indice):
-    lista[indice] = False
-    return lista
+   
  def mostrar_verdaderos(lista):
-    #for i in lista:
-    #    if i == True:
-    #        print(lista.index(i))
-    i = 0
-    while i in range(len(lista)):
-        if lista[i] == True:
-            print(i)
-        i += 1
-
+   
 
 
 def iniciar():
-    posibles_primos = generar_lista_booleanos(30)
-    for i in range(2,31):
-       for j in range(2,31):
-           if i != j:
-               if i%j == 0:
-                   marcar_como_falso(posibles_primos,i)
-    marcar_como_falso(posibles_primos,0)
-    marcar_como_falso(posibles_primos,1)
-    mostrar_verdaderos(posibles_primos)
-
+   
     pass
 
 
