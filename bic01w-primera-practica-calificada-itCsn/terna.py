9335c94a7c497c1a8bcadef603d2b7f65bae59ff
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:53:14 -0500

    Update terna.py

--- bic01w-primera-practica-calificada-itCsn/terna.py
+++ bic01w-primera-practica-calificada-itCsn/terna.py
@@ -1,12 +1,4 @@
 def iniciar():
-    # Ingrese la solucion en las lineas subsiguientes.
-    # INICIO
-    
-    for b in range(1,21):
-        for a in range(1,21):
-            for c in range(1,21):
-                if a**2 == b**2 + c**2 and a>b>c:
-                    print(f"({c}, {b}, {a})")
                 
     # FIN
     return
