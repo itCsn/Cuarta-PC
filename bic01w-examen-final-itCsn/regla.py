9d2bbc9d99520def4ba9c124a87440a0fbc1af56
itCsn <82104936+itCsn@users.noreply.github.com>
2021-07-24 21:57:20 -0500

    Update regla.py

--- bic01w-examen-final-itCsn/regla.py
+++ bic01w-examen-final-itCsn/regla.py
@@ -1,13 +1,4 @@
 def mostrar_regla(numero_pulgadas, longitud_marca_pulgada):
-    aux = longitud_marca_pulgada
-    guion = "-"
-    for i in range(numero_pulgadas + 1):
-        if longitud_marca_pulgada - aux == 0:
-            print(f"{guion * longitud_marca_pulgada}")
-        else:
-            aux = aux -1
-            while (longitud_marca_pulgada - aux) > 0:
-                mostrar_regla(numero_pulgadas, longitud_marca_pulgada - aux) 
                     
 if __name__ == "__main__":
     # mostrar_regla(3, 3)
